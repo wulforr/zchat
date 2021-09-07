b7afc9791ecd3444ba73068e1b65f6d9e42bad3e
Shaurya Vardhan <gsshaurya@gmail.com>
2021-09-07 20:51:45 +0530

    fix: message style in chat for currentUSer

--- src/components/ChatMessages/ChatMessages.jsx
+++ src/components/ChatMessages/ChatMessages.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import "./ChatMessages.css";
 
-export default function ChatMessages({ currentChat, currentUser }) {
+export default function ChatMessages({ currentChat, currentUserId }) {
   const ChatMessagesContainerRef = useRef(null);
 
   // to scroll to the bottom of the chat so that most recent message is in view
   useEffect(() => {
     ChatMessagesContainerRef.current.scrollTop =
       ChatMessagesContainerRef.current.scrollHeight;
-  }, [currentChat]);
+  }, [currentChat.messages]);
 
   return currentChat ? (
     <div className="chat-messages-container" ref={ChatMessagesContainerRef}>
@@ -16,7 +16,7 @@ export default function ChatMessages({ currentChat, currentUser }) {
         <div className="chat-message-wrapper" key={message.messageId}>
           <div
             className={
-              message.sender.id === currentUser.id
+              message.sender.id === currentUserId
                 ? "chat-message-right"
                 : "chat-message-left"
             }
